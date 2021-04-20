8a66152ef95c8b831cd35b1baac398a783338b72
ByamB4 <byamba4life@gmail.com>
2021-04-20 08:16:50 +0800

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -199,8 +199,8 @@ export default {
   },
 
   axios: {
-    baseURL: "http://localhost:8000"
-    // baseURL: "https://oyusec-server.herokuapp.com"
+    // baseURL: "http://localhost:8000"
+    baseURL: "https://oyusec-server.herokuapp.com"
   },
   toast: {
     theme: "bubble",
