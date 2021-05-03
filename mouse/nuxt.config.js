4123588eeb92bd0658fdb7a2b3c87855806e61d6
ByamB4 <byamba4life@gmail.com>
2021-05-03 09:43:38 +0800

    dev: to production [mouse]

--- mouse/nuxt.config.js
+++ mouse/nuxt.config.js
@@ -201,8 +201,8 @@ export default {
   },
 
   axios: {
-    baseURL: "http://localhost:8000"
-    // baseURL: "https://oyusec-server.herokuapp.com"
+    // baseURL: "http://localhost:8000"
+    baseURL: "https://oyusec-server.herokuapp.com"
   },
   pwa: {
     manifest: {
