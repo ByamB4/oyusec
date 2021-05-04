350648f7da4d675b9c82e2f2aecadcffe195655f
ByamB4 <byamba4life@gmail.com>
2021-05-04 22:31:59 +0800

    undo

---
+++ dog/src/layouts/Layout.js
@@ -0,0 +1,13 @@
+import { Grid } from "@material-ui/core";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const Layout = ({ children }) => {
+  return (
+    <div style={{ backgroundColor: "red" }}>
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ dog/src/pages/_app.js
@@ -0,0 +1,12 @@
+import Layout from "../layouts/Layout";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
