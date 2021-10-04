14ef380270ce5c2f14400058253b042ca8287373
byamb4 <byamba4life@gmail.com>
2021-10-04 19:52:56 +0800

    [landing] Added patterns :construction:

--- dog/src/ui/Landing/Hero.tsx
+++ dog/src/ui/Landing/Hero.tsx
@@ -1,61 +1,61 @@
 import React from "react";
-// import Image from "next/image";
+import Image from "next/image";
 import DemoChallenge from "components/DemoChallenge";
-// import { colors } from "configs";
+import { colors } from "configs";
 import {
-  // Box,
+  Box,
   Button,
-  // CircularProgress,
-  // CircularProgressProps,
+  CircularProgress,
+  CircularProgressProps,
   Typography,
-  // circularProgressClasses,
+  circularProgressClasses,
 } from "@mui/material";
 
-// interface ICircularProgressProps extends CircularProgressProps {
-//   className?: string;
-//   backgroundColor?: string;
-//   color?: string | any;
-// }
+interface ICircularProgressProps extends CircularProgressProps {
+  className?: string;
+  backgroundColor?: string;
+  color?: string | any;
+}
 
-// const CusCircularProgress: React.FC<ICircularProgressProps> = (props) => {
-//   const {
-//     className = "",
-//     color = "#6A4BFF",
-//     backgroundColor = "#4E556A",
-//     ...rest
-//   } = props;
+const CusCircularProgress: React.FC<ICircularProgressProps> = (props) => {
+  const {
+    className = "",
+    color = "#6A4BFF",
+    backgroundColor = "#4E556A",
+    ...rest
+  } = props;
 
-//   return (
-//     <Box sx={{ position: "relative" }}>
-//       <CircularProgress
-//         variant="determinate"
-//         sx={{
-//           color: backgroundColor,
-//         }}
-//         size={150}
-//         thickness={4}
-//         {...rest}
-//         value={100}
-//       />
-//       <CircularProgress
-//         variant="determinate"
-//         value={20}
-//         sx={{
-//           color: color,
-//           animationDuration: "550ms",
-//           position: "absolute",
-//           left: 0,
-//           [`& .${circularProgressClasses.circle}`]: {
-//             strokeLinecap: "round",
-//           },
-//         }}
-//         size={150}
-//         thickness={4}
-//         {...rest}
-//       />
-//     </Box>
-//   );
-// };
+  return (
+    <Box sx={{ position: "relative" }}>
+      <CircularProgress
+        variant="determinate"
+        sx={{
+          color: backgroundColor,
+        }}
+        size={150}
+        thickness={4}
+        {...rest}
+        value={100}
+      />
+      <CircularProgress
+        variant="determinate"
+        value={20}
+        sx={{
+          color: color,
+          animationDuration: "550ms",
+          position: "absolute",
+          left: 0,
+          [`& .${circularProgressClasses.circle}`]: {
+            strokeLinecap: "round",
+          },
+        }}
+        size={150}
+        thickness={4}
+        {...rest}
+      />
+    </Box>
+  );
+};
 
 interface Props {
   className?: string;
@@ -63,40 +63,79 @@ interface Props {
 
 const Hero: React.FC<Props> = ({ className = "" }) => {
   const rootRef = React.useRef<HTMLDivElement>(null);
-  // const [dimension, setDimension] = React.useState<{
-  //   width: number;
-  //   height: number;
-  // }>({
-  //   width: 0,
-  //   height: 0,
-  // });
+  const [dimension, setDimension] = React.useState<{
+    width: number;
+    height: number;
+  }>({
+    width: 0,
+    height: 0,
+  });
 
-  // React.useEffect(() => {
-  //   if (rootRef.current) {
-  //     setDimension({
-  //       width: rootRef.current.offsetWidth,
-  //       height: rootRef.current.offsetHeight,
-  //     });
-  //   }
-  // }, []);
+  React.useEffect(() => {
+    if (rootRef.current) {
+      setDimension({
+        width: rootRef.current.offsetWidth,
+        height: rootRef.current.offsetHeight,
+      });
+    }
+  }, []);
 
-  // const randomPosition = (pos: string) => {
-  //   switch (pos) {
-  //     case "y":
-  //       const value = Math.floor(Math.random() * dimension.height);
-  //       if (value > dimension.height / 2) {
-  //         return value - 100;
-  //       }
-  //       return value;
-  //     case "x":
-  //       return 100 + Math.floor(Math.random() * dimension.width);
-  //     default:
-  //       return 0;
-  //   }
-  // };
+  const randomPosition = (pos: string) => {
+    switch (pos) {
+      case "y":
+        const value = Math.floor(Math.random() * dimension.height);
+        if (value > dimension.height / 2) {
+          return value - 100;
+        }
+        return value;
+      case "x":
+        return 100 + Math.floor(Math.random() * dimension.width);
+      default:
+        return 0;
+    }
+  };
 
   return (
-    <div className={`w-full h-full p-0 rounded-3xl ${className}`} ref={rootRef}>
+    <div
+      className={`w-full h-full p-0 rounded-3xl overflow-hidden ${className}`}
+      ref={rootRef}
+    >
+      <div className="">
+        {Array.from({ length: 3 }).map((_, index) => (
+          <div
+            key={index}
+            className={`absolute ${Math.random() < 0.5 && "animate-pulse"}`}
+            style={{
+              top: `${randomPosition("y")}px`,
+              left: `${randomPosition("x")}px`,
+            }}
+          >
+            <Image
+              src={`${process.env.STATIC_ROOT}/img/Home/square-small.png`}
+              width="110px"
+              height="95px"
+              className={`opacity-${(Math.floor(Math.random() * 5) + 1) * 10}`}
+            />
+          </div>
+        ))}
+        {/* {Array.from({ length: 5 }).map((_, index) => (
+          <div
+            key={index}
+            className={`absolute ${Math.random() < 0.5 && "animate-pulse"}`}
+            style={{
+              top: `${randomPosition("y")}px`,
+              left: `${randomPosition("x")}px`,
+            }}
+          >
+            <Image
+              src="/img/Home/line-solid.png"
+              width="10px"
+              height="284px"
+              className={`opacity-${(Math.floor(Math.random() * 3) + 1) * 10}`}
+            />
+          </div>
+        ))} */}
+      </div>
       <div className="grid lg:grid-cols-2 h-full">
         {/* START: Content */}
         <div className="flex flex-col justify-center gap-14 p-10">
@@ -119,53 +158,13 @@ const Hero: React.FC<Props> = ({ className = "" }) => {
         {/* START: Components */}
         <div className="relative lg:block hidden">
           {/* BEGIN: Patterns */}
-          {/* <div className="">
-            {Array.from({ length: 3 }).map((_, index) => (
-              <div
-                key={index}
-                className={`absolute ${Math.random() < 0.5 && "animate-pulse"}`}
-                style={{
-                  top: `${randomPosition("y")}px`,
-                  left: `${randomPosition("x")}px`,
-                }}
-              >
-                <Image
-                  src={`${process.env.STATIC_ROOT}/img/Home/square-small.png`}
-                  width="110px"
-                  height="95px"
-                  className={`opacity-${
-                    (Math.floor(Math.random() * 5) + 1) * 10
-                  }`}
-                />
-              </div>
-            ))}
-            {Array.from({ length: 5 }).map((_, index) => (
-              <div
-                key={index}
-                className={`absolute ${Math.random() < 0.5 && "animate-pulse"}`}
-                style={{
-                  top: `${randomPosition("y")}px`,
-                  left: `${randomPosition("x")}px`,
-                }}
-              >
-                <Image
-                  src="/img/Home/line-solid.png"
-                  width="10px"
-                  height="284px"
-                  className={`opacity-${
-                    (Math.floor(Math.random() * 3) + 1) * 10
-                  }`}
-                />
-              </div>
-            ))}
-          </div> */}
           {/* END: Patterns */}
           <DemoChallenge className={`absolute w-2/3 up-down-object`} />
-          {/* <div
-            className="flex flex-col bg-primary-light1 items-start gap-4 absolute p-4 rounded-xl"
+          <div
+            className="xl:flex hidden flex-col bg-primary-light1 items-start lg:visible invisible gap-4 absolute p-4 rounded-xl"
             style={{
-              top: `50px`,
-              left: `200px`,
+              top: `5%`,
+              left: `0%`,
             }}
           >
             <div className="p-2 rounded-xl relative">
@@ -190,12 +189,12 @@ const Hero: React.FC<Props> = ({ className = "" }) => {
                 </Typography>
               </div>
             </div>
-          </div> */}
-          {/* <div
-            className="flex bg-primary-light1 items-start gap-4 absolute p-4 rounded-xl"
+          </div>
+          <div
+            className="bg-primary-light1 xl:flex hidden items-start gap-4 absolute p-4 rounded-xl"
             style={{
-              top: `300px`,
-              left: `500px`,
+              top: `20%`,
+              left: `35%`,
             }}
           >
             <div className="p-2 rounded-xl relative">
@@ -220,7 +219,7 @@ const Hero: React.FC<Props> = ({ className = "" }) => {
                 </Typography>
               </div>
             </div>
-          </div> */}
+          </div>
         </div>
         {/* END: Components */}
       </div>
