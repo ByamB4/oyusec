abb3dccf3ae06de3d8140637c5d9a3976c5e0481
byamb4 <byamba4life@gmail.com>
2021-10-06 10:40:51 +0800

    [component] Competition Thumbnail hover scale down :art:

--- dog/src/components/CompetitionThumbnail/CompetitionThumbnail.tsx
+++ dog/src/components/CompetitionThumbnail/CompetitionThumbnail.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { ICompetition } from "interfaces";
 import moment from "moment";
 import { safeCompetitionThumbnail } from "utils/safe";
-import Link from 'next/link';
+import Link from "next/link";
 
 interface Props {
   className?: string;
@@ -18,12 +18,13 @@ const CompetitionThumbnail: React.FC<Props> = ({
   return (
     <Link href={`/competitions/${competition.slug}`}>
       <div
-        className={`flex flex-col bg-primary-light1 rounded-xl hover:cursor-pointer ${className}`}
+        className={`flex flex-col bg-primary-light1 rounded-xl hover:cursor-pointer hover:transform hover:scale-90 duration-200 ${className}`}
       >
         <Image
           src={safeCompetitionThumbnail(competition)}
           height={150}
           width={300}
+          objectFit="cover"
         />
         <div className="flex flex-col gap-3 p-4">
           <Typography variant="h4">{competition.name}</Typography>
