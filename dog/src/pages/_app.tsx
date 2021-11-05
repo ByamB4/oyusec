3ddd44ae94af1d166a4addfa87a4194c5a1db4d5
byamb4 <byamba4life@gmail.com>
2021-11-05 05:45:12 -0400

    [page] _app.tsx updated import logic :whale:

--- dog/src/pages/_app.tsx
+++ dog/src/pages/_app.tsx
@@ -3,14 +3,13 @@ import "styles/css/globals.css";
 import "styles/sass/index.sass";
 import Head from "next/head";
 import { AppProps } from "next/app";
-import { ThemeProvider } from "@mui/material/styles";
 import { SnackbarProvider } from "contexts/snackbar";
-import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import { MuiTheme, createEmotionCache } from "initialize";
 import { APP_NAME } from "configs";
 import { SearchProvider } from "contexts/search";
 import { UserProvider } from "contexts/user";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 const clientSideEmotionCache = createEmotionCache();
 
