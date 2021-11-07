13094c310b926e0922fae0eb95e73d0c2774f574
byamb4 <byamba4life@gmail.com>
2021-11-07 08:58:33 -0500

    [controller] challenge return category id :whale:

--- lion/src/controllers/ChallengeController.ts
+++ lion/src/controllers/ChallengeController.ts
@@ -7,7 +7,18 @@ const prisma = new PrismaClient();
 class ChallengeController {
   public async challengeList(_req: Request, res: Response, next: NextFunction) {
     try {
-      const record = await prisma.challenge.findMany({});
+      const record = await prisma.challenge.findMany({
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          category: {
+            select: {
+              id: true,
+            },
+          },
+        },
+      });
       return res.status(200).json({ status: true, data: record });
     } catch (error) {
       next(new HttpException(500, error.message));
@@ -26,7 +37,7 @@ export default () => {
   const controller = new ChallengeController();
   const router = Router();
   router.get('/list', controller.challengeList);
-  router.get('/categories', controller.categoryList);
+  router.get('/category/list', controller.categoryList);
 
   return router;
 };
