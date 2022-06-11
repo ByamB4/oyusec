c2603758b0616f1f09843e518765904bd9f3c816
ByamB4 <byamba4life@gmail.com>
2022-06-11 23:09:46 +0800

    [data] created fake data, and db script

--- butterfly/pkg/db/main.go
+++ butterfly/pkg/db/main.go
@@ -1,10 +1,11 @@
 package db
 
 import (
-	"fmt"
-	"log"
-
 	"butterfly/pkg/model"
+	"fmt"
+
+	"github.com/bxcodec/faker/v3"
+	log "github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -25,6 +26,26 @@ func Init() (*Instance, error) {
 	fmt.Println("DB connection established")
 
 	db.AutoMigrate(&model.Book{})
+	DeleteAllData(&Instance{Gorm: db})
+	Seed(&Instance{Gorm: db})
 
 	return &Instance{Gorm: db}, nil
+}
+
+func Seed(db *Instance) {
+	for i := 0; i < 10; i++ {
+		book := model.Book{
+			Title:  faker.Sentence(),
+			Author: faker.Name(),
+			Desc:   faker.Paragraph(),
+		}
+		db.Gorm.Create(&book)
+	}
+
+	log.Info("DB seeded")
+}
+
+func DeleteAllData(db *Instance) {
+	db.Gorm.Exec("DELETE FROM books")
+	log.Info("DB deleted")
 }
--- butterfly/pkg/model/book.go
+++ butterfly/pkg/model/book.go
@@ -1,7 +1,7 @@
 package model
 
 type Book struct {
-	Id     int    `json:"id" gorm:"primaryKey"`
+	Id     int    `json:"id" gorm:"primaryKey; unique; not null"`
 	Title  string `json:"title"`
 	Author string `json:"author"`
 	Desc   string `json:"desc"`
